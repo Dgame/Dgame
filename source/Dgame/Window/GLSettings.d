a68c3f44ed568faad121b67a1f1b4ff82c17b14f
Randy Schütt <rswhite4@googlemail.com>
2015-03-14 21:14:15 +0100

    Indentation

--- source/Dgame/Window/GLSettings.d
+++ source/Dgame/Window/GLSettings.d
@@ -9,31 +9,31 @@ module Dgame.Window.GLSettings;
  * Author: Randy Schuett (rswhite4@googlemail.com)
  */
 struct GLSettings {
-	/**
-	 * Major Version of OpenGL. 0 means highest possible.
-	 */
-	ubyte majorVersion = 0;
-	/**
-	 * Minor Version of OpenGL. 0 means highest possible.
-	 */
-	ubyte minorVersion = 0;
-	/**
-	 * Anti aliasing level. 0 means deactivated.
-	 * 
-	 * Note: A too high value may crash your application at the beginning
-	 *		 because your driver does not support it.
-	 *
-	 * Typical values are 2, 4 or 8
-	 */
-	ubyte antiAliasLevel = 0;
+    /**
+     * Major Version of OpenGL. 0 means highest possible.
+     */
+    ubyte majorVersion = 0;
+    /**
+     * Minor Version of OpenGL. 0 means highest possible.
+     */
+    ubyte minorVersion = 0;
+    /**
+     * Anti aliasing level. 0 means deactivated.
+     * 
+     * Note: A too high value may crash your application at the beginning
+     *       because your driver does not support it.
+     *
+     * Typical values are 2, 4 or 8
+     */
+    ubyte antiAliasLevel = 0;
 
-	/**
-	 * CTor
-	 */
-	@nogc
-	this(ubyte major, ubyte minor, ubyte antiAlias) pure nothrow {
-		this.majorVersion = major;
-		this.minorVersion = minor;
-		this.antiAliasLevel = antiAlias;
-	}
+    /**
+     * CTor
+     */
+    @nogc
+    this(ubyte major, ubyte minor, ubyte antiAlias) pure nothrow {
+        this.majorVersion = major;
+        this.minorVersion = minor;
+        this.antiAliasLevel = antiAlias;
+    }
 }
