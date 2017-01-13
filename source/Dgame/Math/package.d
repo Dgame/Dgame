c4777c78c14aedd5ded085174054a184adadf616
Leonardo Tada <leonardo_tada@hotmail.com>
2017-01-13 19:28:50 -0200

    Added Circle in the Dgame.Math

--- source/Dgame/Math/package.d
+++ source/Dgame/Math/package.d
@@ -5,6 +5,7 @@ public:
 import Dgame.Math.Geometry;
 import Dgame.Math.Matrix4x4;
 import Dgame.Math.Rect;
+import Dgame.Math.Circle;
 import Dgame.Math.Vector2;
 import Dgame.Math.Vector3;
 import Dgame.Math.Vertex;
