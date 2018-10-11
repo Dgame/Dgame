737290f95e0161de6080fde5a6a850361f7d9047
Windspar <kdrakemagi@gmail.com>
2018-10-10 20:14:49 -0400

    Fix Mix_Init from failing dectection

--- source/Dgame/Window/Internal/Init.d
+++ source/Dgame/Window/Internal/Init.d
@@ -1,23 +1,23 @@
 /*
  *******************************************************************************************
  * Dgame (a D game framework) - Copyright (c) Randy Schütt
- * 
+ *
  * This software is provided 'as-is', without any express or implied warranty.
  * In no event will the authors be held liable for any damages arising from
  * the use of this software.
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
+ *
  * 1. The origin of this software must not be misrepresented; you must not claim
  *    that you wrote the original software. If you use this software in a product,
  *    an acknowledgment in the product documentation would be appreciated but is
  *    not required.
- * 
+ *
  * 2. Altered source versions must be plainly marked as such, and must not be
  *    misrepresented as being the original software.
- * 
+ *
  * 3. This notice may not be removed or altered from any source distribution.
  *******************************************************************************************
  */
@@ -96,7 +96,7 @@ void _initSDL() {
     // Initialize SDL_image
     result = IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
     assert_fmt(result != 0, "Error: Failed to init the required jpg and png support: %s\n", IMG_GetError());
-    
+
     if ((result & IMG_INIT_JPG) == 0)
         print_fmt("Warning: No jpg support: %s\n", IMG_GetError());
     else if ((result & IMG_INIT_PNG) == 0)
@@ -107,19 +107,19 @@ void _initSDL() {
     assert_fmt(result == 0, "Error: SDL_TTF could not be initialized: %s\n", Mix_GetError());
 
     // Initialize SDL_mixer
-    result = Mix_Init(MIX_INIT_OGG | MIX_INIT_MP3);
-    assert_fmt(result != 0, "Error: Failed to init the required ogg and mp3 support: %s\n", Mix_GetError());
-    
-    if ((result & MIX_INIT_OGG) == 0)
-        print_fmt("Warning: No ogg support: %s\n", Mix_GetError());
-    else if ((result & MIX_INIT_MP3) == 0)
-        print_fmt("Warning: No mp3 support: %s\n", Mix_GetError());
-
     result = Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096);
     assert_fmt(result == 0, "Warning: Could not open Mix_OpenAudio: %s\n", Mix_GetError());
 
     SDL_ClearError(); // Ignore XAudio2 error: http://redmine.audacious-media-player.org/issues/346
     
+    result = Mix_Init(MIX_INIT_OGG | MIX_INIT_MP3);
+    assert_fmt(result != 0, "Error: Failed to init the required ogg and mp3 support: %s\n", Mix_GetError());
+
+    if ((result & MIX_INIT_OGG) == 0)
+        print_fmt("Warning: No ogg support: %s\n", Mix_GetError());
+    else if ((result & MIX_INIT_MP3) == 0)
+        print_fmt("Warning: No mp3 support: %s\n", Mix_GetError());
+
     immutable int channels = Mix_AllocateChannels(256);
     if (channels < 256)
         print_fmt("Warning: Could not reserve 256 channels, only %d. %s\n", channels, Mix_GetError());
@@ -192,7 +192,7 @@ void _initGL() {
     immutable GLVersion glver = DerelictGL.reload();
     const char* glverstr = glGetString(GL_VERSION);
     debug print_fmt("Derelict loaded GL version: %d (%d), available GL version: %s\n", DerelictGL.loadedVersion, glver, glverstr);
-    
+
     version (OSX)
         enum GLVersion NEEDED_GL_VERSION = GLVersion.GL21;
     else
